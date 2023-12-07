5dfbbdd80dc366c4da6fe826a2217cb0f47d8991
Cormac-F <cormac.feeney@kainos.com>
2023-12-07 11:34:11 +0000

    Lint fixes

--- controller/jobController.ts
+++ controller/jobController.ts
@@ -1,18 +1,18 @@
 import { Job } from "../model/job";
 import { Request, Response, Application } from "express";
 
-const jobService = require('../service/jobService')
+const jobService = require("../service/jobService");
 
 module.exports = function(app: Application){
-    app.get('/job-roles', async (req: Request, res: Response) => {
+    app.get("/job-roles", async (req: Request, res: Response) => {
         let data: Job[] | undefined;
 
         try {
-            data = await jobService.getJobs()
+            data = await jobService.getJobs();
         } catch (e) {
             console.error(e);
         }
 
-        res.render('list-jobs', { job: data } )
-    })
-}
+        res.render("list-jobs", { job: data } );
+    });
+};
--- service/jobService.ts
+++ service/jobService.ts
@@ -1,12 +1,12 @@
 import { Job } from "../model/job";
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports.getJobs = async function (): Promise<Job> {
     try {
-        const response = await axios.get('http://localhost:8080/api/job-roles')
+        const response = await axios.get("http://localhost:8080/api/job-roles");
 
-        return response.data
+        return response.data;
     } catch (e) {
-        throw new Error('Could not get job')
+        throw new Error("Could not get job");
     }
-}
+};
