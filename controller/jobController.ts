482dc50549b9d8fbea0ffa9f1006e6c822ddcf36
CharlesM988 <charles.mullan@kainos.com>
2023-12-12 11:08:25 +0000

    finalised files for web ui for - US002 - View Job Spec

--- controller/jobController.ts
+++ controller/jobController.ts
@@ -5,18 +5,6 @@ const jobService = require('../service/jobService');
 
 module.exports = function(app: Application){
 
-    app.get("/job-roles", async (req: Request, res: Response) => {
-        let data: Job[] | undefined;
-
-        try {
-            data = await jobService.getJobs();
-        } catch (e) {
-            console.error(e);
-        }
-
-        res.render("list-jobs", { job: data } );
-    });
-
     app.get('/list-jobs/:id', async (req: Request, res: Response) => {
         let data: Job
 
