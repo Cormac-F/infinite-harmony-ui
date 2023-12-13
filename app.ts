c2a4f74d9055ac1db0e4d022f97033289a541d99
Cormac-F <cormac.feeney@kainos.com>
2023-12-13 11:10:14 +0000

    Lint fixes

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
-import { Application, Request, Response } from 'express';
+import { Application, Request, Response } from "express";
 
-const express = require('express');
-const path = require('path');
-const nunjucks = require('nunjucks');
+const express = require("express");
+const path = require("path");
+const nunjucks = require("nunjucks");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 
 // Nunjucks Configuration
-const appViews = path.join(__dirname, '/views/');
+const appViews = path.join(__dirname, "/views/");
 
 function setUpNunjucks(expressApp: Application) {
 
-    let env = nunjucks.configure('views', {
+    const env = nunjucks.configure("views", {
         autoescape: true,
         express: app
     });
@@ -20,24 +20,24 @@ function setUpNunjucks(expressApp: Application) {
 setUpNunjucks(express);
 
 // Express Configuration
-app.set('view engine', 'html');
+app.set("view engine", "html");
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-declare module 'express-session' {
+declare module "express-session" {
     interface SessionData {
     }
 }
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log("Server running on port 3000");
 });
 
 // Express Routes
-app.get('/', async (req: Request, res: Response) => {
-    res.render('index', { title: 'Home' });
+app.get("/", async (req: Request, res: Response) => {
+    res.render("index", { title: "Home" });
 });
