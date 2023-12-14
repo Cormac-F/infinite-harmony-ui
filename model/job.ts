3e8c71b9675642731a57f2c4eaf32c57a436e7ab
amchugh689 <aideen.mchugh@kainos.com>
2023-12-14 11:52:19 +0000

    Added missing semicolon

--- model/job.ts
+++ model/job.ts
@@ -2,7 +2,7 @@ export class Job {
     roleID?: number;
     roleName?: string;
     bandID?: number;
-    bandName?: string
+    bandName?: string;
     familyID?: number;
     specSummary?: string;
     sharepointLink?: string;
