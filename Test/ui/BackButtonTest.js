5bdc79714141b433f62705a6dc0aa5ef680da7f9
CharlesM988 <charles.mullan@kainos.com>
2023-12-20 12:21:17 +0000

    fixing linting mistakes

--- Test/ui/BackButtonTest.js
+++ Test/ui/BackButtonTest.js
@@ -10,22 +10,7 @@ describe("BackButtonTest", () => {
     try {
       await driver.get("http://localhost:3000/");
       await driver.findElement(By.linkText("Employee")).click();
-      const backButton = await driver.findElement(By.className('btn-back'));
-      await backButton.click();
-
-      chai.assert.equal(await driver.getCurrentUrl(), "http://localhost:3000/");
-    } finally {
-      await driver.quit();
-    }
-  });
-
-  it("should click into job role and then back to the job list", async () => {
-    let driver = await new Builder().forBrowser("chrome").build();
-
-    try {
-      await driver.get("http://localhost:3000/job-roles");
-      await driver.findElement(By.linkText("Employee")).click();
-      const backButton = await driver.findElement(By.className('btn-back'));
+      const backButton = await driver.findElement(By.className("btn-back"));
       await backButton.click();
 
       chai.assert.equal(await driver.getCurrentUrl(), "http://localhost:3000/");
