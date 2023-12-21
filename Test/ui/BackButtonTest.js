842d8f5a6d93b484b6cfbd9caf02892247ed163e
CharlesM988 <charles.mullan@kainos.com>
2023-12-21 10:51:58 +0000

    fixing test to align with new changes

--- Test/ui/BackButtonTest.js
+++ Test/ui/BackButtonTest.js
@@ -3,16 +3,15 @@ const chai = require("chai");
 
 const webdriver = require("selenium-webdriver");
 
-describe("BackButtonTest", () => {
+describe("BreadCrumbTest", () => {
   it("should click into employee and then back to the index", async () => {
     let driver = await new Builder().forBrowser("chrome").build();
 
     try {
       await driver.get("http://localhost:3000/");
       await driver.findElement(By.linkText("Employee")).click();
-      const backButton = await driver.findElement(By.className("btn-back"));
-      await backButton.click();
-
+      await driver.findElement(By.id("breadcrumb-home")).click();
+      
       chai.assert.equal(await driver.getCurrentUrl(), "http://localhost:3000/");
     } finally {
       await driver.quit();
