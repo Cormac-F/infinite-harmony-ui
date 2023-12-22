a6900acf81341487e90055d4cc14b9547f08a6dd
CharlesM988 <charles.mullan@kainos.com>
2023-12-22 13:47:57 +0000

    commit to make sure changes are avaliable

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ const path = require("path");
 const nunjucks = require("nunjucks");
 const app = express();
 const session = require("express-session");
+const bodyParser = require("body-parser");
 
 // Nunjucks Configuration
 const appViews = path.join(__dirname, "/views/");
@@ -22,15 +23,19 @@ function setUpNunjucks(expressApp: Application) {
 setUpNunjucks(express);
 
 // Express Configuration
-app.set("view engine", "html");
+app.set("view engine", "html", "ejs");
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({secret: process.env.APP_SECRET_KEY || config.secretKey, cookie: { maxAge: 60000 }}));
+app.use(session({
+    secret: process.env.APP_SECRET_KEY || config.secretKey, cookie: { maxAge: 60000,
+    resave: false,
+    saveUnintialized: true
+}}));
 
 declare module "express-session" {
     interface SessionData {
@@ -48,3 +53,4 @@ app.get("/", async (req: Request, res: Response) => {
 });
 
 require("./controller/jobController")(app);
+require("./controller/authController")(app);
--- controller/authController.ts
+++ controller/authController.ts
@@ -1,9 +1,23 @@
 import { Request, Response, Application } from "express";
 import { Login } from "../model/auth";
+import { SessionData } from "express-session";
+
 const authService = require("../service/authService");
+const session = require("express-session");
+
+interface CustomSessionData extends SessionData {
+    isLoggedIn: boolean;
+}
+
+declare module "express-session" {
+    interface SessionData {
+        isLoggedIn: boolean;
+    }
+}
 
 module.exports = function(app: Application) {
     app.get("/login", async (req: Request, res: Response) => {
+        req.session.isLoggedIn = true
         res.render("login");
     })
 
@@ -13,13 +27,13 @@ module.exports = function(app: Application) {
         try {
             req.session.token = await authService.login(data);
 
-            res.redirect("/product");
+            res.redirect("/job-roles");
         } catch (e) {
             console.error(e);
 
             res.locals.errormessage = e.message;
 
-            res.render("login", req.body)
+            res.render("login", req.body);
         }
     })
 }
