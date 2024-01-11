5d93c598b6d3f8b6f567028ce3a1ffe9b73f1623
CharlesM988 <charles.mullan@kainos.com>
2024-01-11 10:51:03 +0000

    fixes to the login/logout buttons

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -1,4 +1,25 @@
-document.getElementById("logoutButton").addEventListener("click", function() {
+document.getElementById("logoutButtonNavBar").addEventListener("click", function() {
+    fetch("/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+      }),
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = "/";
+      } else {
+        console.error("Logout failed:", response.statusText);
+      }
+    })
+    .catch(error => {
+      console.error("Error during logout:", error);
+    });
+  });
+
+  document.getElementById("logoutButtonIndex").addEventListener("click", function() {
     fetch("/logout", {
       method: "POST",
       headers: {
